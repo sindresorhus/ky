8f17a135f271c114936233b2aef7b253cfc4a493
Ashinze Ekene <ashinzekene@gmail.com>
2019-02-04 12:06:31 +0100

    Actually cancel the request after the timeout (#27)
    Fixes #8 
    
    
    Co-authored-by: Szymon Marczak <36894700+szmarczak@users.noreply.github.com>

--- test/_require.js
+++ test/_require.js
@@ -1,5 +1,7 @@
 import fetch, {Headers, Response} from 'node-fetch';
+import AbortController from 'abort-controller';
 
 global.fetch = fetch;
 global.Headers = Headers;
 global.Response = Response;
+global.AbortController = AbortController;
