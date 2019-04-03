8d295b0072c1733d722a9355f06dd370c469e160
Michael Stramel <stramel@users.noreply.github.com>
2019-04-03 08:27:52 -0500

    Fix Error types (#113)

--- index.d.ts
+++ index.d.ts
@@ -122,13 +122,16 @@ export interface ResponsePromise extends Promise<Response> {
  * The error has a response property with the `Response` object.
  */
 export class HTTPError extends Error {
+	constructor(response: Response);
 	response: Response;
 }
 
 /**
  * The error thrown when the request times out.
  */
-export class TimeoutError extends Error {}
+export class TimeoutError extends Error {
+	constructor();
+}
 
 export interface Ky {
 	/**
