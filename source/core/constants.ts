587cd2c6baf49cd0d6f6be84ae29f7735369d65e
Alexey Berezin <beraliv.spb@gmail.com>
2021-05-04 22:08:14 +0200

    fix: 🧪 ky[method] = ...

--- source/core/constants.ts
+++ source/core/constants.ts
@@ -1,10 +1,15 @@
+import type { Expect, Equal } from '@type-challenges/utils';
 import {HTTPMethod} from '../types/options.js';
 
 export const supportsAbortController = typeof globalThis.AbortController === 'function';
 export const supportsStreams = typeof globalThis.ReadableStream === 'function';
 export const supportsFormData = typeof globalThis.FormData === 'function';
 
-export const requestMethods: readonly HTTPMethod[] = ['get', 'post', 'put', 'patch', 'head', 'delete'];
+export const requestMethods = ['get', 'post', 'put', 'patch', 'head', 'delete'] as const;
+
+type cases = [
+	Expect<Equal<typeof requestMethods[number], HTTPMethod>>,
+]
 
 export const responseTypes = {
 	json: 'application/json',
--- source/index.ts
+++ source/index.ts
@@ -6,14 +6,17 @@ import type {ky as KyInterface} from './types/ky.js';
 import type {Input, Options} from './types/options.js';
 import {validateAndMerge} from './utils/merge.js';
 
+type Mutable<T> = {
+	-readonly[P in keyof T]: T[P]
+};
+
 const createInstance = (defaults?: Partial<Options>): KyInterface => {
 	// eslint-disable-next-line @typescript-eslint/promise-function-async
-	const ky = (input: Input, options?: Options) => Ky.create(input, validateAndMerge(defaults, options));
+	const ky: Partial<Mutable<KyInterface>> = (input: Input, options?: Options) => Ky.create(input, validateAndMerge(defaults, options));
 
 	for (const method of requestMethods) {
-		// @ts-expect-error not sure how to properly type this!
 		// eslint-disable-next-line @typescript-eslint/promise-function-async
-		ky[method] = (input: Input, options: Options) => Ky.create(input, validateAndMerge(defaults, options, {method}));
+		ky[method] = (input: Input, options?: Options) => Ky.create(input, validateAndMerge(defaults, options, {method}));
 	}
 
 	ky.create = (newDefaults?: Partial<Options>) => createInstance(validateAndMerge(newDefaults));
