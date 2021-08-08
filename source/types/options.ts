bfd669b3e3c97ed92a09285718496edc7656801d
robertgr991 <robertgr991@yahoo.ro>
2021-08-09 02:01:44 +0300

    Add note about throwHttpErrors and retry in TS docs

--- source/types/options.ts
+++ source/types/options.ts
@@ -172,6 +172,8 @@ export interface Options extends Omit<RequestInit, 'headers'> {
 	Throw an `HTTPError` when, after following redirects, the response has a non-2xx status code. To also throw for redirects instead of following them, set the [`redirect`](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch#Parameters) option to `'manual'`.
 
 	Setting this to `false` may be useful if you are checking for resource availability and are expecting error responses.
+	
+	Note: If `false`, error responses are considered successful, therefore no retry will have place.	
 
 	@default true
 	*/
