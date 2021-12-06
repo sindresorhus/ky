d5fcecf2ab258e96d881301358d2b829566c12e6
Leon Si <contact@leonzalion.com>
2021-12-06 16:29:22 -0500

    Fix typo
    suppported -> supported

--- source/types/ky.ts
+++ source/types/ky.ts
@@ -112,7 +112,7 @@ export interface KyInstance {
 	// Note that response will be `undefined` in case `ky.stop` is returned.
 	const response = await ky.post('https://example.com', options);
 
-	// Using `.text()` or other body methods is not suppported.
+	// Using `.text()` or other body methods is not supported.
 	const text = await ky('https://example.com', options).text();
 	```
 	*/
