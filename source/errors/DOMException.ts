360b1eec79d7c2d3fdfeab5db94daddaf99eb472
Richie Bendall <richiebendall@gmail.com>
2022-04-21 16:38:22 +1200

    Add todo
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- source/errors/DOMException.ts
+++ source/errors/DOMException.ts
@@ -2,6 +2,7 @@ const DOMException = globalThis.DOMException ?? Error;
 
 export default DOMException;
 
+// When targeting Node.js 18, use `signal.throwIfAborted()` (https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal/throwIfAborted)
 export function composeAbortError(signal?: AbortSignal) {
 	// @ts-expect-error `.reason` is a valid property but is not yet added to the dom typings
 	return new DOMException(signal?.reason ?? 'The operation was aborted.');
