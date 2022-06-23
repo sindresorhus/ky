7fa93d3fd2058eeeb4c8c6ce350df141032369ba
Sindre Sorhus <sindresorhus@gmail.com>
2022-06-23 14:39:31 +0200

    Update Ky.ts

--- source/core/Ky.ts
+++ source/core/Ky.ts
@@ -2,7 +2,7 @@ import {HTTPError} from '../errors/HTTPError.js';
 import {TimeoutError} from '../errors/TimeoutError.js';
 import type {Hooks} from '../types/hooks.js';
 import type {Input, InternalOptions, NormalizedOptions, Options, SearchParamsInit} from '../types/options.js';
-import {ResponsePromise} from '../types/responsePromise.js';
+import {ResponsePromise} from '../types/ResponsePromise.js';
 import {deepMerge, mergeHeaders} from '../utils/merge.js';
 import {normalizeRequestMethod, normalizeRetryOptions} from '../utils/normalize.js';
 import {delay, timeout, TimeoutOptions} from '../utils/time.js';
