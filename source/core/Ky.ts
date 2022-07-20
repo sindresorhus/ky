d48ed955bad7e0d1f6d884164e377766104e669a
Yixuan Xu <yixuanxu94@outlook.com>
2022-07-20 20:16:01 +0800

    Fix copying response body with 204 status code when using `onDownloadProgress` (#444)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- source/core/Ky.ts
+++ source/core/Ky.ts
@@ -285,6 +285,21 @@ export class Ky {
 		const totalBytes = Number(response.headers.get('content-length')) || 0;
 		let transferredBytes = 0;
 
+		if (response.status === 204) {
+			if (onDownloadProgress) {
+				onDownloadProgress({percent: 1, totalBytes, transferredBytes}, new Uint8Array());
+			}
+
+			return new globalThis.Response(
+				null,
+				{
+					status: response.status,
+					statusText: response.statusText,
+					headers: response.headers,
+				},
+			);
+		}
+
 		return new globalThis.Response(
 			new globalThis.ReadableStream({
 				async start(controller) {
