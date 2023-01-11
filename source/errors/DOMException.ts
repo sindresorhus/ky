d1cd6ffe1bd754e03dfc1ecbca8fa47ff2e5e32b
Cristobal Dabed <cristobal@dabed.org>
2023-01-11 15:39:49 +0100

    feat: Throw DomException (or error) with name AbortError

--- source/errors/DOMException.ts
+++ source/errors/DOMException.ts
@@ -1,8 +1,14 @@
-const DOMException = globalThis.DOMException ?? Error;
-
-export default DOMException;
+const domExceptionSupported = Boolean(globalThis.DOMException);
 
 // When targeting Node.js 18, use `signal.throwIfAborted()` (https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal/throwIfAborted)
 export function composeAbortError(signal?: AbortSignal) {
-	return new DOMException(signal?.reason ?? 'The operation was aborted.');
+	// Supported on most modern browsers, and from Node >= 17. (https://developer.mozilla.org/en-US/docs/Web/API/DOMException#browser_compatibility)
+	if (domExceptionSupported) {
+		return new DOMException(signal?.reason ?? 'The operation was aborted.', 'AbortError');
+	}
+
+	const error = new Error(signal?.reason ?? 'The operation was aborted.');
+	error.name = 'AbortError';
+
+	return error;
 }
