2b25a5000052f3881d805555459edf5d5433f189
Cristobal Dabed <cristobal@dabed.org>
2023-01-12 00:29:51 +0100

    fix import type to Implementation from UntitledMacro

--- test/helpers/with-page.ts
+++ test/helpers/with-page.ts
@@ -1,12 +1,12 @@
 import process from 'node:process';
-import type {ExecutionContext, UntitledMacro} from 'ava';
+import type {ExecutionContext, Implementation} from 'ava';
 import {chromium, Page} from 'playwright-chromium';
 
 type Run = (t: ExecutionContext, page: Page) => Promise<void>;
 
 const PWDEBUG = Boolean(process.env['PWDEBUG']);
 
-export const withPage: UntitledMacro<any[]> = async (t: ExecutionContext, run: Run): Promise<void> => {
+export const withPage: Implementation<any[]> = async (t: ExecutionContext, run: Run): Promise<void> => {
 	const browser = await chromium.launch({
 		devtools: PWDEBUG,
 	});
