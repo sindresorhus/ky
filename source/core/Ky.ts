66514479e1da54aa407b04e24af5c7d6c10bfcd4
Martin Donadieu <martindonadieu@gmail.com>
2024-02-24 21:55:46 +0100

    fix: simplify isCredentialsSupported and change typing

--- source/core/Ky.ts
+++ source/core/Ky.ts
@@ -120,8 +120,7 @@ export class Ky {
 		this._input = input;
 		const isCredentialsSupported = 'credentials' in Request.prototype;
 		this._options = {
-			// TODO: credentials can be removed when the spec change is implemented in all browsers. Context: https://www.chromestatus.com/feature/4539473312350208
-			credentials: isCredentialsSupported ? ((this._input as Request).credentials || 'same-origin') : 'same-origin',
+			credentials: isCredentialsSupported ? (this._input as Request).credentials : undefined,
 			...options,
 			headers: mergeHeaders((this._input as Request).headers, options.headers),
 			hooks: deepMerge<Required<Hooks>>(
