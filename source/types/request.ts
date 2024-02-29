c0a4dfa12d3f831b900bb057c3ce97da5f0067d6
Sindre Sorhus <sindresorhus@gmail.com>
2024-02-29 21:46:36 +0700

    Fix missing type dependency
    Fixes #562

--- source/types/request.ts
+++ source/types/request.ts
@@ -1,4 +1,19 @@
-import {type RequestInit as UndiciRequestInit} from 'undici-types';
+type UndiciRequestInit = {
+	method?: string;
+	keepalive?: boolean;
+	headers?: HeadersInit;
+	body?: BodyInit;
+	redirect?: RequestRedirect;
+	integrity?: string;
+	signal?: AbortSignal | undefined;
+	credentials?: RequestCredentials;
+	mode?: RequestMode;
+	referrer?: string;
+	referrerPolicy?: ReferrerPolicy;
+	window?: undefined;
+	dispatcher?: unknown;
+	duplex?: unknown;
+};
 
 type CombinedRequestInit = globalThis.RequestInit & UndiciRequestInit;
 
