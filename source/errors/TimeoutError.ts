4d1984431ef18c117ef04d44f9cecaba1b58acd6
Tom Adler <me@arty.name>
2024-06-19 12:15:29 +0200

    URL only

--- source/errors/TimeoutError.ts
+++ source/errors/TimeoutError.ts
@@ -2,7 +2,7 @@ export class TimeoutError extends Error {
 	public request: Request;
 
 	constructor(request: Request) {
-		super('Request timed out', { cause: request });
+		super('Request timed out', { cause: request.url });
 		this.name = 'TimeoutError';
 		this.request = request;
 	}
