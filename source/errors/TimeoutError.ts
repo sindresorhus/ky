143d084619c8d3fc562fac1dc9fa28ae87279cc1
Tom Adler <me@arty.name>
2024-06-19 12:06:36 +0200

    Add `cause` to `TimeoutError`
    Fixes https://github.com/sindresorhus/ky/issues/592

--- source/errors/TimeoutError.ts
+++ source/errors/TimeoutError.ts
@@ -2,7 +2,7 @@ export class TimeoutError extends Error {
 	public request: Request;
 
 	constructor(request: Request) {
-		super('Request timed out');
+		super('Request timed out', { cause: request });
 		this.name = 'TimeoutError';
 		this.request = request;
 	}
