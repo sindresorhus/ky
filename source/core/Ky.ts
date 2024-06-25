7947d0269155ea87bc3fe373c9ab3a5ca362fcf5
Kenneth Sills <132029135+Kenneth-Sills@users.noreply.github.com>
2024-06-25 05:07:46 +0000

    chore: remove unused import

--- source/core/Ky.ts
+++ source/core/Ky.ts
@@ -1,6 +1,5 @@
 import {HTTPError} from '../errors/HTTPError.js';
 import {TimeoutError} from '../errors/TimeoutError.js';
-import type {Hooks} from '../types/hooks.js';
 import type {Input, InternalOptions, NormalizedOptions, Options, SearchParamsInit} from '../types/options.js';
 import {type ResponsePromise} from '../types/ResponsePromise.js';
 import {mergeHeaders, mergeHooks} from '../utils/merge.js';
